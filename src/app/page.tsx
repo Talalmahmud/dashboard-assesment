d80985014cf6950d0d860798f479edda979e107b
Talal Mahmud <mahmudtalal2@gmail.com>
2024-04-24 07:45:33 +0600

    added meta data on first page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,24 @@
-import Image from "next/image";
-
+import type { Metadata } from "next";
+export const metadata: Metadata = {
+  title: "Dashboard",
+  description: "Dashboard description",
+  openGraph: {
+    title: "Dashboard",
+    description: "Hello.",
+    url: "https://www.davegray.codes/",
+    siteName: "Dashboard",
+    type: "website",
+    images: [
+      {
+        url: "/vercel.svg",
+        secureUrl: "/vercel.svg",
+        width: 1200,
+        height: 630,
+        alt: "Preview image for Dave Gray",
+      },
+    ],
+  },
+};
 export default function Home() {
   return <div>Dashboard</div>;
 }
